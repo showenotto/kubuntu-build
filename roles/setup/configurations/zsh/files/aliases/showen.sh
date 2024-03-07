44d92b1ded7cb3daee63e8c3f76e24bf72d3716f
root <showen@proton.me>
2024-03-07 16:42:25 +0200

    1.0

--- roles/setup/configurations/zsh/files/aliases/showen.sh
+++ roles/setup/configurations/zsh/files/aliases/showen.sh
@@ -1,2 +1,4 @@
 alias showen-backup="~/showen/scripts/showen-backup.sh"
+alias ssd-backup="~/showen/scripts/ssd-backup.sh"
 alias neon-backup="sudo -E ansible-playbook /home/showen/neon-build/backup.yml"
+
